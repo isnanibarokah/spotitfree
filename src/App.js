1ed876e67a249d2a4acff6d8726f747e21dfcc1b
isnanibarokah <isnanibarokah@gmail.com>
2022-03-23 16:20:41 +0700

    Delete div, add br

--- src/App.js
+++ src/App.js
@@ -7,14 +7,13 @@ const SPOTIFY_CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
 
 function App() {
   return (
-    <div className="container">
-      <h1>Playing From Playlist</h1>
-      <div className="boxPlaylist">
-        <img src={data.album.images[0].url} alt="ImagePlaylist" />
-        <h3>{data.name}</h3>
-        <p>{data.album.artists[0].name}</p>
-        <button className="btnSelect">Select</button>
-      </div>
+    <div className="boxPlaylist">
+      <h2>Playing From Playlist</h2>
+      <img src={data.album.images[0].url} alt="ImagePlaylist" />
+      <h3>{data.name}</h3>
+      <p>{data.album.artists[0].name}</p>
+      <br></br>
+      <button className="btnSelect">Select</button>
     </div>
   );
 }
