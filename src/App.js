2da8bf20c46a0210dcc46963a84bc00949bfa99e
isnanibarokah <isnanibarokah@gmail.com>
2022-03-25 15:10:23 +0700

    Modify to render track component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import data from './data.js';
+import "./App.css";
+import data from "./data.js";
+import Track from "./components/track";
 
-
-const SPOTIFY_CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+// const SPOTIFY_CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
 
 function App() {
   return (
-    <div className="boxPlaylist">
-      <h2>Playing From Playlist</h2>
-      <img src={data.album.images[0].url} alt="ImagePlaylist" />
-      <h3>{data.name}</h3>
-      <p>{data.album.artists[0].name}</p>
-      <br></br>
-      <button className="btnSelect">Select</button>
+    <div className="container">
+      <Track
+        url={data.album.images[0].url}
+        title={data.name}
+        artist={data.album.artists[0].name}
+      />
     </div>
   );
 }
