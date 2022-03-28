d8da76ec30fb3576980e3207370670cac84e1bc7
isnanibarokah <isnanibarokah@gmail.com>
2022-03-28 16:23:37 +0700

    Move title to App.js

--- src/components/track/index.js
+++ src/components/track/index.js
@@ -1,7 +1,6 @@
 const Track = ({ url, title, artist }) => {
     return (
       <div className="boxPlaylist">
-        <h2>Playing From Playlist</h2>
         <img src={url} alt="Track Playlist" />
         <h3>{title}</h3>
         <p>{artist}</p>
