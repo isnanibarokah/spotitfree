350524582c93c6f5bef3debcf3e44a411d5fe2ef
isnanibarokah <isnanibarokah@gmail.com>
2022-03-30 21:44:51 +0700

    change return to Home

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import "./App.css";
-import data from "./data.js";
-import Track from "./components/track";
+import Home from "./pages/home";
 
 // const SPOTIFY_CLIENT_ID = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
 
 function App() {
-  const trackList = data.map((track) => (
-    <Track
-      key={track.id}
-      url={track.album.images[0].url}
-      title={track.name}
-      artist={track.artists[0].name}
-    />
-  ));
-
-  return (
-    <div className="container">
-      <h2>Playing From Playlist</h2>
-      <div className="trackList">{trackList}</div>
-    </div>
-  );
+  return <Home/>
 }
 
 export default App;
