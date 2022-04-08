72544661e8c3677a5c800417ae95e30f0a569bfd
isnanibarokah <isnanibarokah@gmail.com>
2022-04-08 11:27:34 +0700

    Rename const

--- src/components/searchbar/index.js
+++ src/components/searchbar/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { searchTrack } from "../../setup/fetchApi";
 
-export default function Searchbar({ onSuccess, onClearSearch }) {
+export default function SearchBar({ onSuccess, onClearSearch }) {
   const [text, setText] = useState("");
   const accessToken = useSelector((state) => state.auth.accessToken);
 
