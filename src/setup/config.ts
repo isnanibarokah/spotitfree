5cd3e2013e2646f626f5264a7f5e0118637e1cc1
isnanibarokah <isnanibarokah@gmail.com>
2022-04-24 00:19:31 +0700

    update config

--- src/setup/config.ts
+++ src/setup/config.ts
@@ -13,7 +13,7 @@ interface IConfig {
     SPOTIFY_BASE_URL: 'https://api.spotify.com/v1',
     SPOTIFY_SCOPE: 'playlist-modify-private',
     RESPONSE_TYPE: 'token',
-    REDIRECT_URI: 'https://spotitfree.vercel.app/callback/',
+    REDIRECT_URI: 'http://localhost:3000',
   };
   
   export default config;
